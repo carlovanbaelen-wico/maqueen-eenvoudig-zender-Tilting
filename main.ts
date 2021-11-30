6a2ad2b6311d83462bb7b53412d9f6e3436972c6
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2021-11-30 23:02:02 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -9,19 +9,19 @@ input.onButtonPressed(Button.B, function () {
 })
 radio.setGroup(1)
 basic.forever(function () {
-    if (input.acceleration(Dimension.X) > 500) {
+    if (input.isGesture(Gesture.TiltRight)) {
         basic.clearScreen()
         led.plot(4, 2)
         radio.sendNumber(4)
-    } else if (input.acceleration(Dimension.X) < -500) {
+    } else if (input.isGesture(Gesture.TiltLeft)) {
         basic.clearScreen()
         led.plot(0, 2)
         radio.sendNumber(3)
-    } else if (input.acceleration(Dimension.Y) > 500) {
+    } else if (input.isGesture(Gesture.LogoUp)) {
         basic.clearScreen()
         led.plot(2, 4)
         radio.sendNumber(2)
-    } else if (input.acceleration(Dimension.Y) < -500) {
+    } else if (input.isGesture(Gesture.LogoDown)) {
         basic.clearScreen()
         led.plot(2, 0)
         radio.sendNumber(1)
